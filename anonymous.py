2fb3837f19fe4038720447b43c11a3d666414f9a
Jackey <smjjackey@sina.com>
2020-01-29 17:15:55 +0800

    Add Lambda practice
    Signed-off-by: Jackey <smjjackey@sina.com>

---
+++ anonymous.py
@@ -0,0 +1,11 @@
+"""
+Lambda practice for https://www.liaoxuefeng.com/wiki/1016959663602400/1017451447842528
+"""
+def is_odd(n):
+    return n % 2 == 1
+
+L = list(filter(is_odd, range(1, 20)))
+print(L)
+
+L= list(filter(lambda n:n%2==1, range(1, 20)))
+print(L)
