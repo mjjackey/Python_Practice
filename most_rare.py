49f1fcf21ca1670c1a1ed3507aa28eed88ccbc19
Jackey <smjjackey@sina.com>
2020-02-08 15:10:24 +0800

    1.Add n_most_rare
    Signed-off-by: Jackey <smjjackey@sina.com>

---
+++ most_rare.py
@@ -0,0 +1,30 @@
+"""
+Based on the given integer n, list, return the element whose  frequency of occurrence
+is equal to n
+"""
+class Rare:
+    def nth_most_rare(elements,n):
+        if(not isinstance(elements,list)):
+            print("not list")
+            return
+        if(not isinstance(n,int)):
+            print("not int")
+            return
+        x={}
+        for ele in elements:
+            if str(ele) not in x.keys():  # str
+                x.update({str(ele):1})  # str
+            else:
+                x[str(ele)] += 1
+            # print(x)
+        element=list(x.keys())[list(x.values()).index(n+1)]
+        return element
+
+n=Rare.nth_most_rare([5,4,3,2,1,5,4,3,2,5,4,3,5,4,5],4)
+print(n)
+
+n=Rare.nth_most_rare((5,4,3,2,1,5,4,3,2,5,4,3,5,4,5),4)
+print(n)
+
+n=Rare.nth_most_rare([5,4,3,2,1,5,4,3,2,5,4,3,5,4,5],4.5)
+print(n)
