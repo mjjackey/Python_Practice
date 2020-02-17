2fd9807fcc49a70e117ef190aca83dff9953ceac
Jackey <smjjackey@sina.com>
2020-02-17 18:00:06 +0800

    Add another method for q1 file
    Signed-off-by: Jackey <smjjackey@sina.com>

--- Huawei/2019/q1.py
+++ Huawei/2019/q1.py
@@ -9,16 +9,16 @@ def fun():
     input_num_list=list(map(int,input().strip().split(',')))
     out=set()
     for num in input_num_list:
-        if(num !=1 and all(num%i !=0 for i in range(2,int(math.sqrt(num))+1))):
-            ##### Return True if all elements of the iterable are true (or if the iterable is empty)
-            ##### So, if the input_num_list contain {1}, the result is wrong
-            out.add(num)
-        # flag= True
-        # for i in range(2,int(math.sqrt(num))+1):
-        #     if (num%i):
-        #         flag= False
-        #         break
-        # if(flag and num!=1): out.add(num)
+        # if(num !=1 and all(num%i !=0 for i in range(2,int(math.sqrt(num))+1))):
+        #     ##### Return True if all elements of the iterable are true (or if the iterable is empty)
+        #     ##### So, if the input_num_list contain {1}, the result is wrong
+        #     out.add(num)
+        flag= True
+        for i in range(2,int(math.sqrt(num))+1):
+            if (num%i==0):
+                flag= False
+                break
+        if(flag and num!=1): out.add(num)
 
     out=sorted(out)
     if(len(out)==0):
