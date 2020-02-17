cdbb74f2bdf8e56be4e894ee0bcb8126076f4a3a
Jackey <smjjackey@sina.com>
2020-02-17 17:58:15 +0800

    Modify the q1 and prime_number file bugs.
    Signed-off-by: Jackey <smjjackey@sina.com>

--- Huawei/2019/q1.py
+++ Huawei/2019/q1.py
@@ -1,16 +1,25 @@
 """
 Accept a line of string set split by comma, the number of string <1000, pick the un-repeated prime number,
 sort ascending,output them; if no prime number,output "empty"
+e.g.
+1,2,3,3,5,7,9
 """
 import math
 def fun():
     input_num_list=list(map(int,input().strip().split(',')))
     out=set()
     for num in input_num_list:
-        if(all(num%i !=0 for i in range(2,int(math.sqrt(num))+1))):
+        if(num !=1 and all(num%i !=0 for i in range(2,int(math.sqrt(num))+1))):
             ##### Return True if all elements of the iterable are true (or if the iterable is empty)
-            ##### So, if the input_num_list is {1,2}, the result is wrong
+            ##### So, if the input_num_list contain {1}, the result is wrong
             out.add(num)
+        # flag= True
+        # for i in range(2,int(math.sqrt(num))+1):
+        #     if (num%i):
+        #         flag= False
+        #         break
+        # if(flag and num!=1): out.add(num)
+
     out=sorted(out)
     if(len(out)==0):
         print("empty")
@@ -22,7 +31,7 @@ def fun2():
     input_num_list=list(map(int,input().strip().split(',')))
     out=[]
     for num in input_num_list:
-        if(all(num%i !=0 for i in range(2,int(math.sqrt(num))+1))):
+        if(num !=1 and all(num%i !=0 for i in range(2,int(math.sqrt(num))+1))):
             out.append(num)
     out=sorted(set(out))
     if(len(out)==0):
--- prime_number.py
+++ prime_number.py
@@ -3,14 +3,17 @@ def generate_prime():
     n=100
     pri_num_list=[]
     for num in range(2, n + 1): # the range of candidate numbers
-        if all(num % j != 0 for j in range(2, int(math.sqrt(num))+1)):  # Try divide 2 to sqrt(num)
+        if all(num % j != 0 for j in range(2, int(math.sqrt(num))+1)):
+            # Try divide 2 to sqrt(num)
+            ##### Actually, for num=2 or num=3, inner loop does not do, the all returns true
+            ##### because if the iterable is empty, all still return true,but 2,3 are prime
             pri_num_list.append(num)
 
     print(len(pri_num_list),pri_num_list)
 
 def generate_prime1():
     n=100
-    pri_num_list = [num for num in range(2, n + 1) if all(num % j != 0 for j in range(2, int(math.sqrt(num)) + 1))]
+    pri_num_list = [num for num in range(2, n + 1) if all(num % j != 0 for j in range(2, int(math.sqrt(num)+0.5) + 1))]
     print(len(pri_num_list), pri_num_list)
 
 def generate_prime2():
@@ -18,7 +21,7 @@ def generate_prime2():
     pri_num_list = []
     for num in range(2,n+1): # the range of candidate numbers
         flag = True
-        for j in range(2, int(math.sqrt(num)+1)):  # Try divide 2 to sqrt(num)
+        for j in range(2, int(math.sqrt(num)+0.5)+1):  # Try divide 2 to sqrt(num)
             if(num%j==0):
                 flag=False
                 break
@@ -32,7 +35,7 @@ def generate_prime3():
     for num in range(2, n + 1):
         flag1 = True
         for j in range(2, int(math.sqrt(num) + 1)): # Try divide 2 to sqrt(num)
-            flag2=True
+            flag2= True
             ### check whether j is a prime, if j is not a prime, continue,
             # because if the number can be divided by a not prime, it must can be divided by the prime
             # number between 2~10 i.e. 2,3,5,7###
@@ -40,7 +43,7 @@ def generate_prime3():
                 if (j % k == 0):
                     flag2 = False # j is not a prime
                 break
-            if(flag2==False):
+            if(not flag2):
                 continue
             ### check whether j is a prime ###
             
