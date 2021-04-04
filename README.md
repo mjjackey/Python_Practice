1c48de37cca20c728a8cf1b2b1ce2a026aaf52b2
Jackey <mjjackey@163.com>
2021-04-04 17:09:54 +0800

    1. Update the "reademe"
    Signed-off-by: Jackey <mjjackey@163.com>

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Python_Practice
-The miscellaneous practices for python
+The miscellaneous practices for python, which contains some 
+high order features and functional programming, i.e. 
+
+- generator
+- iterator
+
+- map-reduce
+- filter
+- sorted 
+
+- closure
+- lambda
+- decorator
+
+In addition, this practice contains some coding tests for some companies.
+and some data-structures and algorithms. 
--- sorted.py
+++ sorted.py
@@ -1,5 +1,8 @@
 """
 The sorted function practice is for https://www.liaoxuefeng.com/wiki/1016959663602400/1017408670135712
+假设我们用一组tuple表示学生名字和成绩：
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+请用sorted()对上述列表分别按名字排
 """
 # -*- coding: utf-8 -*-
 L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
@@ -7,17 +10,20 @@ L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
 def by_name(t):
     return t[0].lower()
 
-L1 = sorted(L, key=by_name)
-print(L1)
-
-L1=sorted(L,key=lambda t:t[0])
-print(L1)
 
 def by_score(t):
     return t[1]
 
-L2 = sorted(L, key=by_score,reverse=True)
-print(L2)
+if __name__ == "__main__":
+    L1 = sorted(L, key=by_name)
+    print(L1)
 
-L2= sorted(L,key=lambda t:t[1],reverse=True)
-print(L2)
+    L1 = sorted(L, key=lambda t: t[0])
+    print(L1)
+
+
+    L2 = sorted(L, key=by_score,reverse=True)
+    print(L2)
+
+    L2= sorted(L,key=lambda t:t[1],reverse=True)
+    print(L2)
