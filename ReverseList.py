caa48e92efc5902ce36e9270db54d2cc86f51415
Jackey <mjjackey@163.com>
2025-01-12 12:02:23 +0800

    1. Add ReverseList.py and traversal_directory.py
    Signed-off-by: Jackey <mjjackey@163.com>

---
+++ ReverseList.py
@@ -0,0 +1,26 @@
+"""
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+"""
+class ListNode():
+    def __init__(self,x):
+        self.value = x
+        nextNode = ListNode()
+        self.next = nextNode
+
+def ReverseList(pHead):
+    pReverseHead = None
+
+
+
+
+
+if __name__=="__main__":
+    pass
--- decorator_log_var.py
+++ decorator_log_var.py
@@ -4,7 +4,6 @@ Two layer packages
 Three layer packages for pass self-defined parameters
 """
 import functools
-
 def log(para):
     if(not isinstance(para,str)):
         def wrapper(*args, **kw):
@@ -13,7 +12,7 @@ def log(para):
         return wrapper
     else:
         def decorator(func):
-            # @functools.wraps(func)
+            @functools.wraps(func)
             def wrapper(*args, **kw):
                 print('%s %s():' % (para, func.__name__))
                 return func(*args, **kw)
@@ -32,3 +31,4 @@ def f2():
 if __name__ == "__main__":
     f1()
     f2()
+    print(f2.__name__)
